c3b121fd8f14a0b01ba6224fd98f50c52e8c5fa6
Irina Kriuchkova <ivk2005@yandex.ru>
2024-01-08 10:07:48 +0300

    project created

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-export default function characterHealthIndicate(characterHealth) {
-  if (characterHealth.health > 50) {
-    return ('healthy');
-  } else if (characterHealth.health <= 50 && characterHealth.health >= 15) {
-      return ('wounded');
-    } else {
-        return ('critical');
-    }
-}
